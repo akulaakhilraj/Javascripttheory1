24d2f76f7a18ef305566e60fa720c63dfc6dcc24
Akhil <akulaakhilraj@gmail.com>
2025-04-30 16:27:15 -0400

    Call Back Function

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Javascripttheory1
 Javascript
+
+
+# What is a Call Back Function? 
+Callbacks are functions passed as arguments to another function to be executed later, often used in asynchronous operations. They are typically used to handle the result of an asynchronous operation once it completes.
+
+Callbacks can also be used for error handling in asynchronous operations.
+
+
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Javascript")
+//Call back Functions
+function playergame(callback){
+    setTimeout(()=>{
+        callback("Need to enter the game play");
+    },1000)
+}
+
+playergame((game)=>{
+    console.log(game)
+})
+
+function playergame1(callback){
+    setTimeout(()=>{
+        const error=null;
+        const winner="player won the game"
+        callback(error,winner);
+    },1000)
+}
+
+playergame1((error,winner)=>{
+    if(error){
+        console.log("Error:", error);
+    }
+    else{
+        console.log("Winner :", winner);
+    }
+})
