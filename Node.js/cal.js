9ee911563113128511d21e6a19c8a911ea0f6456
Akhil <akulaakhilraj@gmail.com>
2025-05-22 22:40:02 -0400

    Javascript Notes document

---
+++ Node.js/cal.js
@@ -0,0 +1,14 @@
+
+//{
+// import {sumofPlayer} from "./sum.js"
+
+// const a=10;
+// const b=20;
+
+// sumofPlayer(a,b);
+// }
+
+
+const {sumofPlayer}= require("./sum");
+
+sumofPlayer(10,6);
---
+++ Node.js/sum.js
@@ -0,0 +1,14 @@
+// {
+// export function sumofPlayer(a,b){
+//     const sum=a+b;
+//     console.log(sum);
+// }
+// }
+
+
+function sumofPlayer(a,b){
+    const sum=a+b;
+    console.log(sum);
+}
+
+module.exports={sumofPlayer};
